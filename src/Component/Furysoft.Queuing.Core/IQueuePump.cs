68839f13c2234efd8168df7999606d2cffe84799
Simon Paramore <simon.paramore@emailhippo.com>
2018-06-21 09:14:26 +0100

    Added methods to pump

--- src/Component/Furysoft.Queuing.Core/IQueuePump.cs
+++ src/Component/Furysoft.Queuing.Core/IQueuePump.cs
@@ -6,10 +6,30 @@
 
 namespace Furysoft.Queuing.Core
 {
+    using System.Threading;
+    using JetBrains.Annotations;
+
     /// <summary>
     /// Pump for submitting queue messages
     /// </summary>
     public interface IQueuePump
     {
+        /// <summary>
+        /// Adds the message.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="message">The message.</param>
+        void AddMessage<TEntity>([NotNull] TEntity message);
+
+        /// <summary>
+        /// Starts the Queue Pump.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        void Start(CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Stops the queue pump.
+        /// </summary>
+        void Stop();
     }
 }
